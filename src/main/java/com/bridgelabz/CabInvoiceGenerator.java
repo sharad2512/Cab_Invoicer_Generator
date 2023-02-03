025f0b2a2c4b739bd8fc52506bc9173c13500100
sharad2512 <shnarute95@gmail.com>
2023-02-03 10:58:32 +0530

    ADDED : Code to Calculate Fare for multiple Rides

--- src/main/java/com/bridgelabz/CabInvoiceGenerator.java
+++ src/main/java/com/bridgelabz/CabInvoiceGenerator.java
@@ -2,7 +2,16 @@ package com.bridgelabz;
 public class CabInvoiceGenerator {
     private static final int MINIMUM_COST_PER_KILOMETER = 10;
     private static final int COST_PER_TIME = 1;
+    public double calculateFare(Ride[] rides) {
+        double totalFare = 0;
+        for (Ride ride : rides) {
+
+            totalFare += this.calculateFare(ride.getDistance(), ride.getTime());
+        }
+        return totalFare;
+    }
     public static double calculateFare(double distance, int time) {
+
         return distance * MINIMUM_COST_PER_KILOMETER + time * COST_PER_TIME;
     }
     public static void main(String[] args) {
---
+++ src/main/java/com/bridgelabz/Ride.java
@@ -0,0 +1,15 @@
+package com.bridgelabz;
+public class Ride {
+    private double distance;
+    private int time;
+    public Ride(double distance, int time) {
+        this.distance = distance;
+        this.time = time;
+    }
+    public double getDistance() {
+        return distance;
+    }
+    public int getTime() {
+        return time;
+    }
+}
