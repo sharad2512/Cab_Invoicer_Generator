d265ed394283746dbee82119961051da60e03786
sharad2512 <shnarute95@gmail.com>
2023-02-03 10:34:34 +0530

    Welcome to Cab Invoice Generator Program

---
+++ src/main/java/com/bridgelabz/CabInvoiceGenerator.java
@@ -0,0 +1,6 @@
+package com.bridgelabz;
+public class CabInvoiceGenerator {
+    public static void main(String[] args) {
+        System.out.println("Welcome to cab invoice generator program");
+    }
+}
